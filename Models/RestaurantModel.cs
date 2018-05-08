e323f8f2924d8a5c5e78933f15544fff19327f78
Monica <mmille16@cbu.edu>
2018-05-07 21:37:55 -0400

    initial commit

---
+++ Models/RestaurantModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantWeb1.Models
+{
+    public class RestaurantModel
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Requires name")]
+        public string name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public int AvgRating { get; set; }
+    }
+}
